b80fc8bc54ea32bd58919f6e8a5de2125f1d7e8d
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2022-05-09 09:45:59 -0300

    dialogflow

---
+++ lib/widgets/chat_message_list_item.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: unused_element
+
+import 'package:chatbot/models/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatMessageListItem extends StatelessWidget {
+  final ChatMessage chatMessage;
+
+  // ignore: use_key_in_widget_constructors
+  const ChatMessageListItem({required this.chatMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return chatMessage.type == ChatMessageType.sent
+        ? _showSentMessage()
+        : _showReceivedMessage();
+  }
+
+  Widget _showSentMessage({EdgeInsets? padding, TextAlign? textAlign}) {
+    return ListTile(
+      contentPadding: const EdgeInsets.fromLTRB(64.0, 0.0, 8.0, 0.0),
+      trailing: CircleAvatar(child: Text(chatMessage.name.toUpperCase()[0])),
+      title: Text(chatMessage.name, textAlign: TextAlign.right),
+      subtitle: Text(chatMessage.text, textAlign: TextAlign.right),
+    );
+  }
+
+    Widget _showReceivedMessage() {
+    return ListTile(
+      contentPadding: const EdgeInsets.fromLTRB(8.0, 0.0, 64.0, 0.0),
+      leading: CircleAvatar(child: Text(chatMessage.name.toUpperCase()[0])),
+      title: Text(chatMessage.name, textAlign: TextAlign.left),
+      subtitle: Text(chatMessage.text, textAlign: TextAlign.left),
+    );
+  }
+}
